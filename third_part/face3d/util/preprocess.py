267f0d4c93fc25f52a2ff42428badfe9dcdbc239
Tschulayski <154610669+Tschulayski@users.noreply.github.com>
2026-02-16 14:57:18 +0100

    Update preprocess.py

--- third_part/face3d/util/preprocess.py
+++ third_part/face3d/util/preprocess.py
@@ -10,9 +10,8 @@ import os
 from skimage import transform as trans
 import torch
 import warnings
-warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
+warnings.filterwarnings("ignore")
 warnings.filterwarnings("ignore", category=FutureWarning) 
-
 
 # calculating least square problem for image alignment
 def POS(xp, x):
